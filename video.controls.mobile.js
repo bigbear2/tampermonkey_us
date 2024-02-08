9be35a20065f1e047f41a189fff6e5bb4e8544c7
Fabio Lucci <fabio.lucci.it@gmail.com>
2024-02-08 02:03:03 +0100

    1

--- video.controls.mobile.js
+++ video.controls.mobile.js
@@ -2,7 +2,7 @@
 
 // @name                Video Mobile Fabio L.
 // @description         Controls any HTML5 video
-// @version             0.18
+// @version             0.17
 
 // @namespace           io.bigbear2.video.mobile
 // @include             *
